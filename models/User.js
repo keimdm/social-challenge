bb41a42cda429509391c468411cc68c544addab5
David Keim <dmk252@cornell.edu>
2023-04-04 17:03:56 -0400

    updated user model, added thought model

--- models/User.js
+++ models/User.js
@@ -5,13 +5,24 @@ const userSchema = new Schema(
   {
     username: { type: String, required: true, unique: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true, match: /.+\@.+\..+/, },
-    thoughts: [String],
-    friends: [String],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: {
       virtuals: true,
-    }
+    },
+    id: false,
   }
 );
 
